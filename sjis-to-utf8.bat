a0d7203d695021caa7ef6f4e62f072eaa9cdca85
eirblaze <eirblaze@live.jp>
2019-02-20 00:11:44 +0900

    コマンドメモ

--- sjis-to-utf8.bat
+++ sjis-to-utf8.bat
@@ -1,3 +1,5 @@
+# utf8に置き換え
+
 Get-ChildItem D:\SDK\XAMPP\xampp\htdocs\WebHold\yahoo-e\html `
 -Recurse -Include *.css | `
 Rename-Item -NewName {$_.FullName + ".bkup"}
@@ -12,4 +14,8 @@ ForEach-Object { `
 
 
 Remove-Item D:\SDK\XAMPP\xampp\htdocs\WebHold\yahoo-e\html\**\*.bkup -Recurse
+
+
+# あとからgitignore。プロジェクトルートから実行すること。
+git rm --cached $(git ls-files --full-name -i --exclude-from=.gitignore)
 
