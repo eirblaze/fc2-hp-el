759315a1c444f56398905129cbe8b3ca9f6652f3
eirblaze <eirblaze@live.jp>
2019-02-20 07:16:12 +0900

    プラグインなし版のwebpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,28 @@
 const entry_js = `./_src/js/index.js`;
 const output_js = './html/js/script.min.js'
 
+const merge = require('webpack-merge');
+
+
 module.exports = (env, argv) => {
 
   //console.log(env);
   //console.log(argv);
 
+  var is_dev = (argv.mode === 'development');
+  //console.log(is_dev);
+
   var return_modules = {}
 
   // devtool
-  if (argv.mode === "production") {
-    return_modules = Object.assign(return_modules,{
-      devtool: 'source-map',
+  if (is_dev) {
+    return_modules = merge(return_modules,{
+      devtool: 'source-map'
     });
   }
 
   // 入出力
-  return_modules = Object.assign(return_modules,{
+  return_modules = merge(return_modules,{
 
     // メインとなるJavaScriptファイル（エントリーポイント）
     entry: entry_js,
