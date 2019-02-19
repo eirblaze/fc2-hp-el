656fecb4f3331787810b7df036a5b91fe1658e95
eirblaze <eirblaze@live.jp>
2019-02-19 23:08:09 +0900

    sjis-utf8

---
+++ sjis-to-utf8.bat
@@ -0,0 +1,4 @@
+Get-ChildItem D:\SDK\XAMPP\xampp\htdocs\WebHold\yahoo-e\html -Recurse -Include *.html | Rename-Item -NewName {$_.FullName + ".bkup"}
+
+
+Get-ChildItem D:\SDK\XAMPP\xampp\htdocs\WebHold\yahoo-e\html -Recurse -Include *.html.bkup | ForEach-Object {Get-Content $_.FullName | Out-File -Encoding UTF8 ($_.FullName -replace '.html.bkup','.html')}
