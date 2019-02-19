dad808caf6a20bbadeca9b4b681c2529c948d91a
eirblaze <eirblaze@live.jp>
2019-02-20 07:01:50 +0900

    js準備中

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,38 @@
-module.exports = arg => {
+const entry_js = `./_src/js/index.js`;
+const output_js = './html/js/script.min.js'
 
-  console.log(arg);
+module.exports = (env, argv) => {
+
+  //console.log(env);
+  //console.log(argv);
 
   var return_modules = {}
 
+  // devtool
+  if (argv.mode === "production") {
+    return_modules = Object.assign(return_modules,{
+      devtool: 'source-map',
+    });
+  }
+
   // 入出力
-  Object.assign(return_modules,{
+  return_modules = Object.assign(return_modules,{
 
     // メインとなるJavaScriptファイル（エントリーポイント）
-    entry: `./_src/index.js`,
+    entry: entry_js,
 
     // ファイルの出力設定
     output: {
       // 出力ファイル名
-      filename: 'html/js/script.min.js'
+      path: __dirname,
+      filename: output_js
     },
 
+    mode: argv.mode
+
   });
+
+  console.log(return_modules);
 
   return return_modules;
 };
