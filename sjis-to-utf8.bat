72239b23547fd59449ece5469e415881d019ce19
eirblaze <eirblaze@live.jp>
2019-02-19 23:20:35 +0900

    バックアップ削除

--- sjis-to-utf8.bat
+++ sjis-to-utf8.bat
@@ -1,4 +1,13 @@
-Get-ChildItem D:\SDK\XAMPP\xampp\htdocs\WebHold\yahoo-e\html -Recurse -Include *.html | Rename-Item -NewName {$_.FullName + ".bkup"}
+Get-ChildItem D:\SDK\XAMPP\xampp\htdocs\WebHold\yahoo-e\html `
+-Recurse -Include *.html | `
+Rename-Item -NewName {$_.FullName + ".bkup"}
 
 
-Get-ChildItem D:\SDK\XAMPP\xampp\htdocs\WebHold\yahoo-e\html -Recurse -Include *.html.bkup | ForEach-Object {Get-Content $_.FullName | Out-File -Encoding UTF8 ($_.FullName -replace '.html.bkup','.html')}
+Get-ChildItem D:\SDK\XAMPP\xampp\htdocs\WebHold\yahoo-e\html `
+-Recurse -Include *.html.bkup | `
+ForEach-Object { `
+  Get-Content $_.FullName | `
+  Out-File -Encoding UTF8 ($_.FullName -replace '.html.bkup','.html') `
+}
+
+Remove-Item D:\SDK\XAMPP\xampp\htdocs\WebHold\yahoo-e\html\*.bkup -Recurse
