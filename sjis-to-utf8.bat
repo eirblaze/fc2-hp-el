d19ee91ec01bf18f76d8b5bcca7ac7464d0a204f
eirblaze <eirblaze@live.jp>
2019-02-20 00:36:41 +0900

    Revert "js-renamer"
    This reverts commit 29d7538127baa2fdac3b1e3bb39fa2712d964325.

--- sjis-to-utf8.bat
+++ sjis-to-utf8.bat
@@ -1,19 +1,19 @@
 # utf8に置き換え
 
 Get-ChildItem D:\SDK\XAMPP\xampp\htdocs\WebHold\yahoo-e\html `
--Recurse -Include *.java | `
+-Recurse -Include *.css | `
 Rename-Item -NewName {$_.FullName + ".bkup"}
 
 
 Get-ChildItem D:\SDK\XAMPP\xampp\htdocs\WebHold\yahoo-e\html `
--Recurse -Include *.java | `
+-Recurse -Include *.css.bkup | `
 ForEach-Object { `
   Get-Content $_.FullName | `
-  Out-File -Encoding UTF8 ($_.FullName -replace '.java','.js') `
+  Out-File -Encoding UTF8 ($_.FullName -replace '.css.bkup','.css') `
 }
 
 
-Remove-Item D:\SDK\XAMPP\xampp\htdocs\WebHold\yahoo-e\html\**\*.java -Recurse
+Remove-Item D:\SDK\XAMPP\xampp\htdocs\WebHold\yahoo-e\html\**\*.bkup -Recurse
 
 
 # あとからgitignore。プロジェクトルートから実行すること。
