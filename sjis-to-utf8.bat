29d7538127baa2fdac3b1e3bb39fa2712d964325
eirblaze <eirblaze@live.jp>
2019-02-20 00:36:31 +0900

    js-renamer

--- sjis-to-utf8.bat
+++ sjis-to-utf8.bat
@@ -1,19 +1,19 @@
 # utf8に置き換え
 
 Get-ChildItem D:\SDK\XAMPP\xampp\htdocs\WebHold\yahoo-e\html `
--Recurse -Include *.css | `
+-Recurse -Include *.java | `
 Rename-Item -NewName {$_.FullName + ".bkup"}
 
 
 Get-ChildItem D:\SDK\XAMPP\xampp\htdocs\WebHold\yahoo-e\html `
--Recurse -Include *.css.bkup | `
+-Recurse -Include *.java | `
 ForEach-Object { `
   Get-Content $_.FullName | `
-  Out-File -Encoding UTF8 ($_.FullName -replace '.css.bkup','.css') `
+  Out-File -Encoding UTF8 ($_.FullName -replace '.java','.js') `
 }
 
 
-Remove-Item D:\SDK\XAMPP\xampp\htdocs\WebHold\yahoo-e\html\**\*.bkup -Recurse
+Remove-Item D:\SDK\XAMPP\xampp\htdocs\WebHold\yahoo-e\html\**\*.java -Recurse
 
 
 # あとからgitignore。プロジェクトルートから実行すること。
