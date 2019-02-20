f7bf21374746a10a3876d92e8891dd3188596e1b
firblaze <38304392+firblaze@users.noreply.github.com>
2019-02-20 09:48:54 +0900

    upgrade webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const entry_js = `./_src/js/index.js`;
-const output_js = './html/js/script.min.js'
+const dist_path = './html/';
+const output_js = 'js/script.min.js'
 
 const webpack = require('webpack');
 const merge = require('webpack-merge');
@@ -31,8 +33,8 @@ module.exports = (env, argv) => {
     // ファイルの出力設定
     output: {
       // 出力ファイル名
-      path: __dirname,
-      filename: output_js
+      filename: output_js,
+      path: path.resolve(__dirname, dist_path),
     },
 
     mode: argv.mode
@@ -58,6 +60,14 @@ module.exports = (env, argv) => {
                   '@babel/preset-env',
                 ]
               }
+            },
+            options: {
+              envName: argv.mode, // optionsのenvNameでBabelのモードを指定。省略するとBabelはdevelopmentモードで呼び出される
+              comments: is_dev,
+              presets: [
+                // プリセットを指定することで、ES2018 を ES5 に変換
+                '@babel/preset-env',
+              ]
             }
           ]
         }
@@ -67,13 +77,12 @@ module.exports = (env, argv) => {
 
 
   return_modules = merge(return_modules,{
-    plugins: [
-      new webpack.ProvidePlugin(
-        {
-          jQuery: "jquery",
-          $: "jquery",
-        }
-      ),
+    // 外部にホスティングされているjQueryなどのパッケージを読み込んで使用する方法 http://elsur.xyz/webpack-jquery-ways-to-work#jQueryundefined
+    externals: [
+      {
+        jquery: 'jQuery',
+        //vegas: 'vegas'
+      }
     ],
   });
 
