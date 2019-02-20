e56090b26602a3cc3990ebf35e1dd71fa73194ce
eirblaze <eirblaze@live.jp>
2019-02-20 22:57:53 +0900

    Revert "引数指定でリセット可能にしたけどうまくいかない"
    This reverts commit 2c70f14e6c20096af54076468e6123a7f2eac34e.

--- _src/js/yahoo/fin.js
+++ _src/js/yahoo/fin.js
@@ -16,26 +16,28 @@
   ];
 
   // タイマー処理 @link https://techacademy.jp/magazine/5541
-  var f_bg_fade_in__count = (i_count = 0) => {
+  var count = 0;
+  var f_bg_fade_in__count = () => {
 
-    console.log(i_count);
-    console.log(colors[i_count]);
+    console.log(count);
+    console.log(colors[count].color);
 
     $('body').css({
-      'backgroundColor' : colors[i_count].color
+      'backgroundColor' : colors[count].color
     });
 
     // タイマーが満了した後に実行 @link https://developer.mozilla.org/ja/docs/Web/API/WindowTimers/setTimeout
-    var timer_id = setTimeout( f_bg_fade_in__count(i_count + 1), colors[i_count].next_delay * 1 );
+    var timer_id = setTimeout( f_bg_fade_in__count, colors[count].next_delay * 1 );
 
     // 限界まで来たらタイマー停止
-    if(i_count >= colors.length){
+    count++;
+    if(count >= colors.length){
       clearTimeout(timer_id); // timer_idをclearTimeoutで指定している
     }
   }
 
   $(document).ready(f_bg_fade_in__count);
-  $('button, a').on("click", f_bg_fade_in__count);
+  $('button').on("click", f_bg_fade_in__count);
 
 
 
