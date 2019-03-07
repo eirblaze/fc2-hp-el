ab494199c98666e525e124d51e0596ead50411a5
eirblaze <eirblaze@live.jp>
2019-03-07 22:03:19 +0900

    プラグインは外しておく

--- webpack.config.js
+++ webpack.config.js
@@ -85,8 +85,7 @@ module.exports = (env, argv) => {
     // 外部にホスティングされているjQueryなどのパッケージを読み込んで使用する方法 http://elsur.xyz/webpack-jquery-ways-to-work#jQueryundefined
     externals: [
       {
-        jquery: 'jQuery',
-        //vegas: 'vegas'
+        jquery: 'jQuery'
       }
     ],
 
