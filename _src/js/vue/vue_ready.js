2b1fcd4061ce3a8fcf0bad2b24eac1d90ac8ab12
eirblaze <eirblaze@gmail.com>
2019-04-17 23:07:50 +0900

    共通関数を適用

--- _src/js/vue/vue_ready.js
+++ _src/js/vue/vue_ready.js
@@ -1,3 +1,5 @@
+import * as Common from "../common";
+
 /**
  * Vue準備
  *
@@ -14,7 +16,7 @@ export function vue_ready(i__vue_args) {
 
     // Vue対象を取得
     // 存在しなければ終了
-    if ( ! jq_alive( $(i__vue_args.el) ) ) return {}
+    if ( ! Common.jq_alive( $(i__vue_args.el) ) ) return {}
 
     // eslint-disable no-new -- ESLint は JavaScript のための静的検証ツールです。 https://qiita.com/mysticatea/items/f523dab04a25f617c87d
     return new Vue(i__vue_args)
@@ -22,26 +24,6 @@ export function vue_ready(i__vue_args) {
 
     // ローカル関数 --------------------------------------------------------------------------------------------
 
-    /**
-     * jQueryオブジェクトを受け取り、中身が存在するかどうかチェック。
-     * 存在するときtrueを返す。
-     *
-     * @param {jQueryオブジェクト} $__dom
-     */
-    function jq_alive($__dom) {
-      //console.log($(i__vue_args.el));
-
-      // 存在しないとき(デフォルト)
-      let ret__jq_alive = false
-
-      // 存在するときは、デフォルトを書き換える。
-      if ( $__dom.length > 0 ) {
-        ret__jq_alive = true
-      }
-
-      // 結果を返す
-      return ret__jq_alive
-    }
 
   })(jQuery);
 }
