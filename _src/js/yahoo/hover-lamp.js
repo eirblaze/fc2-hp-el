6b6f2409dc91912aaacf1ca80376e88724a79a0c
eirblaze <eirblaze@gmail.com>
2019-06-01 18:04:59 +0900

    マウスホバーでランプを付ける仕掛けのベースをコピー

---
+++ _src/js/yahoo/hover-lamp.js
@@ -0,0 +1,74 @@
+﻿/*	b..js
+
+ * 画像切り替え用スクリプト
+
+ * 関数は看板ごとに、個別に必要となる
+
+ */
+
+
+
+img1 = new Image();
+
+img1.src = 'img/bl2.gif';
+
+img2 = new Image();
+
+img2.src = 'img/bl1.gif';
+
+
+
+function mmsa(n){
+
+document.gmma.src = ("img/bl"+n+".gif")
+
+}
+
+
+
+function mmsb(n){
+
+document.gmmb.src = ("img/bl"+n+".gif")
+
+}
+
+
+
+function mmsc(n){
+
+document.gmmc.src = ("img/bl"+n+".gif")
+
+}
+
+
+
+function mmsd(n){
+
+document.gmmd.src = ("img/bl"+n+".gif")
+
+}
+
+
+
+function mmse(n){
+
+document.gmme.src = ("img/bl"+n+".gif")
+
+}
+
+
+
+function mmsy(n){
+
+document.gmmy.src = ("img/bl"+n+".gif")
+
+}
+
+
+
+function mmsz(n){
+
+document.gmmz.src = ("img/bl"+n+".gif")
+
+}
+
