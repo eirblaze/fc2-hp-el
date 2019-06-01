79efa76f9287333db01f846a41bba33450594b88
eirblaze <eirblaze@gmail.com>
2019-06-01 18:45:13 +0900

    js側セレクタのところまで終了

--- _src/js/site-parts/hover-lamp.js
+++ _src/js/site-parts/hover-lamp.js
@@ -1,68 +1,45 @@
-﻿import SiteData from './../data/site-data.json';
+﻿import * as Common from "../common";
+import SiteData from './../data/site-data.json';
+
+( ($) => {
+  // コンフリクト回避
+
+  // 読み込み終了後に実行を予約
+  $(document).ready( () => {
+
+    // DOM
+    let $lampGroups = $('.hover-lamp');
+    if ( !Common.jq_alive($lampGroups) ) return;
+
+    for (var group in $lampGroups) {
+      group.$lamp = $('.hover-lamp__lamp');
+      group.$senser = $('.hover-lamp__switch');
+    }
+
+    // 画像オブジェクト設定
+    const lampImgs = {
+      hover: SiteData.baseUrl + '/img/bl2.gif',
+      unhover: SiteData.baseUrl + '/img/bl1.gif'
+    };
+
+    function toHoverImg($jq){
+      $jq.css({
+        backgroundImage: lampImgs.hover,
+      });
+    }
+
+    function toUnHoverImg($jq){
+      $jq.css({
+        backgroundImage: lampImgs.unhover,
+      });
+    }
+
+    // ホバーイベント
+    $senser.on('mouseover');
 
 
+    // ホバー解除イベント
+    $senser.on('mouseout');
 
-img1 = new Image();
-
-img1.src = 'img/bl2.gif';
-
-img2 = new Image();
-
-img2.src = 'img/bl1.gif';
-
-
-
-function mmsa(n){
-
-document.gmma.src = ("img/bl"+n+".gif")
-
-}
-
-
-
-function mmsb(n){
-
-document.gmmb.src = ("img/bl"+n+".gif")
-
-}
-
-
-
-function mmsc(n){
-
-document.gmmc.src = ("img/bl"+n+".gif")
-
-}
-
-
-
-function mmsd(n){
-
-document.gmmd.src = ("img/bl"+n+".gif")
-
-}
-
-
-
-function mmse(n){
-
-document.gmme.src = ("img/bl"+n+".gif")
-
-}
-
-
-
-function mmsy(n){
-
-document.gmmy.src = ("img/bl"+n+".gif")
-
-}
-
-
-
-function mmsz(n){
-
-document.gmmz.src = ("img/bl"+n+".gif")
-
-}
-
+  });
+})(jQuery);
