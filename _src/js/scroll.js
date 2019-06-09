ea758eae54cf1a18646168554ab70492f70f1797
eirblaze <eirblaze@gmail.com>
2019-06-09 15:27:39 +0900

    とりあえずスクロールのやつは保留

--- _src/js/scroll.js
+++ _src/js/scroll.js
@@ -8,7 +8,7 @@
       // ページ内リンクかどうかを判別。ページ内ではないときは、リンクを通常動作にする。
       console.log("link: " + event.target.href);
       console.log(location.href);
-      return false;
+      return;
 
       // ここからコピー @see https://qiita.com/Takuya_Kouyama/items/b815eb5e1f85d819b4d8
       var speed = 400;
