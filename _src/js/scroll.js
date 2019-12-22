595c023367a096f2c0a7f067e6bff4318037dbb6
eirblaze <eirblaze@gmail.com>
2019-12-22 11:17:28 +0900

    スムーズスクロールはとりあえずそのまま

--- _src/js/scroll.js
+++ _src/js/scroll.js
@@ -7,8 +7,8 @@
     $('a').click(function(event) {
       // ページ内リンクかどうかを判別。ページ内ではないときは、リンクを通常動作にする。
       console.log(`link: ${event.target.href}`)
-console.log(location.href)
-      return
+      console.log(location.href)
+      //return
 
       // ここからコピー @see https://qiita.com/Takuya_Kouyama/items/b815eb5e1f85d819b4d8
       const speed = 400
