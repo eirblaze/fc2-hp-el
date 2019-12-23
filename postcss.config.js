c6d746eaf6ec22906be86659303d23cd677ee6a2
eirblaze <eirblaze@gmail.com>
2019-12-24 01:41:03 +0900

    原則webpack制御だがenv取れるようにもしとく

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,18 @@
-module.exports = {
-  plugins: {
-    'autoprefixer': {
-      overrideBrowserslist: [
-        'last 1 version',
-        '> 1%',
-        'ie 11'
-      ],
-      grid: "autoplace",
-    },
-    'postcss-normalize-charset': {},
-    'cssnano': {autoprefixer: false},
+module.exports = (env) => {
+  console.log("post-css-log")
+  console.log(env)
+  return {
+    plugins: {
+      'autoprefixer': {
+        overrideBrowserslist: [
+          'last 1 version',
+          '> 1%',
+          'ie 11'
+        ],
+        grid: "autoplace",
+      },
+      'postcss-normalize-charset': {},
+      'cssnano': {autoprefixer: false},
+    }
   }
 }
